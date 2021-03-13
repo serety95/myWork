cf2d2398b7b1b1f9bb47ead1c3441433a0d14938
serety95 <serety95@gmail.com>
2021-03-13 14:30:09 +0200

    services add

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,13 @@ import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { PortfolioComponent } from './portfolio/portfolio.component';
+import { ServicesComponent } from './services/services.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent},
   { path: 'about', component: AboutComponent },
   { path: 'portfolio', component: PortfolioComponent },
+  { path: 'services', component: ServicesComponent },
   { path: '**', component: PageNotFoundComponent }
 ];
 
