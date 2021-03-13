f7e8474a3a15877a95329f86d13b562826ef93c9
serety95 <serety95@gmail.com>
2021-03-13 18:30:57 +0200

    serv

--- src/app/services/services.component.ts
+++ src/app/services/services.component.ts
@@ -1,12 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-
+import {faGem} from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-services',
   templateUrl: './services.component.html',
   styleUrls: ['./services.component.scss']
 })
 export class ServicesComponent implements OnInit {
-
+faGem=faGem;
   constructor() { }
 
   ngOnInit(): void {
